7c2804d31319068b2f58f62eaf8da1a6393d18c0
Pawan Kumar <37031863+pawan-subudhi@users.noreply.github.com>
2019-06-15 01:41:09 +0530

    Add files via upload

---
+++ DataStructures/StacksAndQueues/04_queueUsingLinkedList.c
@@ -0,0 +1,58 @@
+//Whatever you are trying to return from dequeue function to show it as an error should not be a member of queue.
+//enqueue id done at the end of linked list and deletion is done at front of linked list.
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+	int i;
+	struct node *link;
+};
+struct node *head=NULL;
+void enqueue(int item){
+	struct node *temp=(struct node*)malloc(sizeof(struct node));
+	if(temp==NULL){
+		printf("Malloc error\n");
+		return;
+	}
+	temp->i=item;
+	temp->link=NULL;
+	if(head==NULL)
+		head=temp;
+	else{
+		struct node *p=head;
+		while(p->link!=NULL)
+			p=p->link;
+		p->link=temp;
+	}
+}
+int dequeue(){
+	struct node *p;
+	if(head==NULL){
+		printf("Underflow\n");
+		return -1;
+	}
+	int item=head->i;
+	p=head;
+	head=head->link;
+	free(p);
+	return item;
+}
+int main(){
+	int i,ele;
+	printf("Enter the slno for the corresponding operation\n");
+	while(1){
+		printf("1:Enqueue\n2:Dequeue\n3:Exit\nEnter the operation value::");
+		scanf("%d",&i);
+		if(i==1){
+			printf("Enter the element to be enqueued into the queue::");
+			scanf("%d",&ele);
+			enqueue(ele);
+		}
+		else if(i==2)
+			printf("the dequeued element is::%d\n",dequeue());
+		else if(i==3)
+			break;
+		else
+			printf("Invalid Input\n");
+	}
+	return 0;
+}
